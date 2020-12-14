6c295f705384c1a553690c1e7393b88273c11392
Przemek Ambroży <przemek19986@gmail.com>
2020-12-14 23:14:17 +0100

    Initial Commit

---
+++ HangmanClient/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  HangmanClient
+//
+//  Created by Przemek Ambroży on 14/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
